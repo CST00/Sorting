e1f944d450f91fa3d6e6eee6f28f5725c8f0d38f
Caden Trent <83717327+CST00@users.noreply.github.com>
2021-11-04 02:03:54 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,11 +2,10 @@
 python3 499Sort.py
 if diff ExpectedS1.txt Sorted.txt then
 	echo "S1 Pass"
-Else
+else
 	echo"S1 Fail"
 if diff Expected2.txt Sorted2.txt; then
 	echo "S2 Pass"
 else
 	"S2 Failed"
-fi
-	
+fi	
