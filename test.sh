dd6e63aa171ddf5468d70fffc48846c87ece299e
Caden Trent <83717327+CST00@users.noreply.github.com>
2021-11-05 00:58:44 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if diff ExpectedS1.txt Sorted.txt; then
-	true
+	echo "s1 passed"
 else
 	exit(1)
 fi
 if diff ExpectedS2.txt Sorted2.txt; then
-	true
+	echo "s1 passed"
 else
 	exit(1)
 fi
