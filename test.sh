422838399363c1236f1a04e0a22f505ea2fcc385
Caden Trent <83717327+CST00@users.noreply.github.com>
2021-11-05 01:01:28 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+import sys
 if diff ExpectedS1.txt Sorted.txt; then
 	echo "s1 passed"
 else
-	exit(1)
+	sys.exit(1)
 fi
 if diff ExpectedS2.txt Sorted2.txt; then
 	echo "s1 passed"
 else
-	exit(1)
+	sys.exit(1)
 fi
